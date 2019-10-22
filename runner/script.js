879ae9d783f36e43cab805efaa830a3099faa626
YunisHuseynzade <yunishuseynzade1102@gmail.com>
2019-10-22 21:50:30 +0400

    improved design object

--- runner/script.js
+++ runner/script.js
@@ -1,1 +1,4 @@
-writeln(math.power(15,2))
+var arr = ["HOME","CONTACT","ABOUT ME"];
+design.add.navbar(arr,"default");
+//Showing a label
+design.add.label("This is a auto website","grey","20px","font-family:monospace;letter-spacing:0.5px;");
