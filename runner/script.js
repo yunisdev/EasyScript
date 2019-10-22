de20ec8b1e27445560185d4f7e27ad3c03c35f3c
YunisHuseynzade <yunishuseynzade1102@gmail.com>
2019-10-22 21:56:53 +0400

    some changes

--- runner/script.js
+++
@@ -1,4 +0,0 @@
-var arr = ["HOME","CONTACT","ABOUT ME"];
-design.add.navbar(arr,"default");
-//Showing a label
-design.add.label("This is a auto website","grey","20px","font-family:monospace;letter-spacing:0.5px;");
